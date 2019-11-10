9bce389a9c4bc2da09580136f628a95050fdbc90
Duc Vu <de.yd315@gmail.com>
2019-11-10 11:29:38 -0600

    change url linnk

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:8000");
+const socket = openSocket("https://ducchatapp.herokuapp.com/");
 
 const subscribeToTimer = cb => {
   console.log("subscribe to timer");

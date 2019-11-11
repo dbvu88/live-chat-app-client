b77eaae901a24716dd94ee4afbe2d8e07ff6dc44
Duc Vu <de.yd315@gmail.com>
2019-11-10 19:58:36 -0600

    refactored code

--- src/api.js
+++ src/api.js
@@ -6,23 +6,19 @@ const url = process.env.REACT_APP_SERVER_URL || "http://localhost:8000/";
 const socket = openSocket(url);
 // "https://ducchatapp.herokuapp.com/"
 const subscribeToTimer = cb => {
-  console.log("subscribe to timer");
+  // console.log("subscribe to timer");
   socket.on("timer", cb);
   socket.emit("subscribeToTimer", 1000);
 };
 
 const handleNewMessage = cb => {
-  console.log("subscribe to newMessage");
+  // console.log("subscribe to newMessage");
   socket.on("conversation", cb);
 };
 
 const emitNewMessage = newMessage => {
-  console.log("on newMessage");
+  // console.log("on newMessage");
   socket.emit("newMessage", newMessage);
 };
-
-// const subscribeToNewUser = cb => {
-//   socket.on("newUser", cb);
-// };
 
 export { subscribeToTimer, handleNewMessage, emitNewMessage };
